4d1918a25d1f05d09d3eed75a7e9e8fd1713c44d
Devon Hackley <devonhackley@gmail.com>
2019-05-14 16:12:01 -0700

    got pic route working

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const superagent = require('superagent');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/'});
 const pg = require('pg');
+const bodyParser = require('body-parser')
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('./public'));
 
@@ -29,50 +30,33 @@ function Fortune (username, fortune, lotto, dominant_attribute, score, created_o
 
 
 app.get('/', (request, response) => response.send('Server works'));
-app.get('/pic', (request, response) => {
-  try {
-    facePlusAPICall(request)
-      .then( FaceObj => {
-        return response.status(200).send(FaceObj);
-      });
-  } catch (error) {
-    console.log('error line 37');
-    response.status(500).send('hello');
-  }
-});
+app.post('/pic',upload.single('image'), (request, response) => sendPic(request, response));
+
+function sendPic(req, res) {
+  const queryData = req.body.imageObj; //this should be the base64string
+  facePlusAPICall(queryData);
+}
 
 function facePlusAPICall (imgData) {
   try {
-    // let facePlusQueryString = `https://api-us.faceplusplus.com/facepp/v3/detect?api_key=${process.env.FACEPLUS_API_KEY}&api_secret=${process.env.FACEPLUS_API_KEY}&image_base64=${imgData}&return_attributes=emotion`
-    let facePlusQueryString = `https://api-us.faceplusplus.com/facepp/v3/detect?api_key=jkbjMP6Jl9y3rdGU-6F86EAS3PbheD6w&api_secret=5_Yq5fGE9sPfdjjYRlaHpmXIqVVwa8lK&image_url=https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Waldemar_Ritter_2015.jpg/325px-Waldemar_Ritter_2015.jpg&return_attributes=emotion`
+    let facePlusQueryString =`https://api-us.faceplusplus.com/facepp/v3/detect?api_key=${process.env.FACEPLUS_API_KEY}&api_secret=${process.env.FACEPLUS_API_SECRET}`;
     return superagent
       .post(facePlusQueryString)
-      .then( (faceAPIRes) => {
-        console.log(faceAPIRes.body.faces[0].attributes.emotion.anger);
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send({image_base64: imgData, return_attributes: 'emotion'})
+      .then((faceAPIRes) => {
+        console.log(faceAPIRes.body.faces[0]);
         let faceAPIInstance = new Fortune ('adminTest', 'This is a dummy fortune', '43, 2, 5, 23, 45, 21', 'dummy', 'dummy', Date.now());
         console.log(faceAPIInstance);
         return faceAPIInstance;
-      });
+      })
+      .catch(err => console.log(err))
     
   } catch(error) {
     response.status(500).send('Sorry all berries');
   }
 }
-app.post('/mult',upload.single('image'), (request, response) => sendPic(request, response));
 
-function sendPic(req, res) {
-  const queryData = req.body.imageObj; //this should be the base64string
-  // console.log('works', req.body);
-  //Todo make request to face++
-superagent.post('https://api-us.faceplusplus.com/facepp/v3/detect')
-        .send({ api_key: process.env.FACE_PLUS_API_KEY, api_secret: process.env.FACE_PLUE_API_SECRET,image_base64: queryData })
-        .then(result => {
-          console.log(result)
-          res.send('works')
-        })
-        .catch(err => console.error(err))
-
-}
 
 
 app.use('*', (request, response) => response.send('Oops'));
