000800d6b5b3abb23b40440c48a4b3ab193b1e9a
Devon Hackley <devonhackley@gmail.com>
2019-05-15 13:39:51 -0700

    fortunes route

--- server.js
+++ server.js
@@ -30,25 +30,42 @@ function Fortune (username, fortune, lotto, dominant_attribute, score, created_o
 
 
 app.get('/', (request, response) => response.send('Server works'));
-app.post('/pic',upload.single('image'), (request, response) => sendPic(request, response));
+app.post('/pic',upload.single('image'), (request, response) => facePlusAPICall(request, response));
+app.get('/fortunes', (request, response) => grabFortunes(request, response));
 
-function sendPic(req, res) {
-  const queryData = req.body.imageObj; //this should be the base64string
-  facePlusAPICall(queryData);
+function grabFortunes(req, res){
+  // query sql table for data based on username
+  let sqlStatement = 'SELECT * FROM users WHERE username=$1';
+  let values = [req.query.data.username];
+  return client.query(sqlStatement, values)
+    // send back data 
+    .then(result => {
+      let fortuneArr = [];
+      if(result.rowCount > 0) {
+        fortuneArr = result.rows[0].map(fortune => {
+          new Fortune(fortune.username, fortune.fortune, fortune.lotto, fortune.dominant_attribute, fortune.score, fortune.created_on)
+        })
+        return res.send(fortuneArr)
+      } else {
+        return res.send('You do not have any fortunes');
+      }
+    })
+    .catch(err => res.status(500).send('Sorry an error ocurred trying to grab your fortunes'))
 }
 
-function facePlusAPICall (imgData) {
+function facePlusAPICall (req, res) {
   try {
+    let imgData = req.body.imageObj;
     let facePlusQueryString =`https://api-us.faceplusplus.com/facepp/v3/detect?api_key=${process.env.FACEPLUS_API_KEY}&api_secret=${process.env.FACEPLUS_API_SECRET}`;
     return superagent
       .post(facePlusQueryString)
       .set('Content-Type', 'application/x-www-form-urlencoded')
       .send({image_base64: imgData, return_attributes: 'emotion'})
       .then((faceAPIRes) => {
-        console.log(faceAPIRes.body.faces[0]);
-        let faceAPIInstance = new Fortune ('adminTest', 'This is a dummy fortune', '43, 2, 5, 23, 45, 21', 'dummy', 'dummy', Date.now());
-        console.log(faceAPIInstance);
-        return faceAPIInstance;
+        console.log(faceAPIRes.body.faces[0].attributes.emotion);
+        let faceAPIInstance = new Fortune ('adminTest', 'This is a dummy fortune', '43, 2, 5, 23, 45, 21', 'dummy', faceAPIRes.body.faces[0].attributes.emotion, Date.now());
+        // console.log(faceAPIInstance);
+        return res.send(faceAPIInstance);
       })
       .catch(err => console.log(err))
     
