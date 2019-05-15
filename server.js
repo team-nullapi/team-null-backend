6b6846ed6af17a7f9f2a55e61ded42bb9b6e7fd7
Timothy Joseph Busch <timbusch1@gmail.com>
2019-05-15 11:32:23 -0700

    added linter on my end refactored some of the instantiation

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const superagent = require('superagent');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/'});
 const pg = require('pg');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -51,40 +51,41 @@ app.get('/pic', (request, response) => {
 function facePlusAPICall (imgData) {
   try {
     // let facePlusQueryString =`https://api-us.faceplusplus.com/facepp/v3/detect?api_key=${process.env.FACEPLUS_API_KEY}&api_secret=${process.env.FACEPLUS_API_SECRET}`;
-    let facePlusQueryString = `https://api-us.faceplusplus.com/facepp/v3/detect?api_key=jkbjMP6Jl9y3rdGU-6F86EAS3PbheD6w&api_secret=5_Yq5fGE9sPfdjjYRlaHpmXIqVVwa8lK&image_url=https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Waldemar_Ritter_2015.jpg/325px-Waldemar_Ritter_2015.jpg&return_attributes=emotion`
+    let facePlusQueryString = `https://api-us.faceplusplus.com/facepp/v3/detect?api_key=jkbjMP6Jl9y3rdGU-6F86EAS3PbheD6w&api_secret=5_Yq5fGE9sPfdjjYRlaHpmXIqVVwa8lK&image_url=https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Waldemar_Ritter_2015.jpg/325px-Waldemar_Ritter_2015.jpg&return_attributes=emotion`;
     return superagent
       .post(facePlusQueryString)
       // .set('Content-Type', 'application/x-www-form-urlencoded')
       // .send({image_base64: imgData, return_attributes: 'emotion'})
       .then((faceAPIRes) => {
-        let faceAPIInstance = new Fortune ('adminTest', fortunePicker('anger'), lotto(), dominantAttribute(faceAPIRes.body), faceAPIRes.body.faces[0].attributes.emotion.sadness, faceAPIRes.body.faces[0].attributes.emotion.neutral, faceAPIRes.body.faces[0].attributes.emotion.disgust, faceAPIRes.body.faces[0].attributes.emotion.anger, faceAPIRes.body.faces[0].attributes.emotion.surprise, faceAPIRes.body.faces[0].attributes.emotion.fear, faceAPIRes.body.faces[0].attributes.emotion.happiness, Date.now());
+        let emotionsObj = faceAPIRes.body.faces[0].attributes.emotion;
+        let faceAPIInstance = new Fortune ('adminTest', fortunePicker('anger'), lotto(), dominantAttribute(faceAPIRes.body), emotionsObj.sadness, emotionsObj.neutral, emotionsObj.disgust, emotionsObj.anger, emotionsObj.surprise, emotionsObj.fear, emotionsObj.happiness, Date.now());
         console.log(faceAPIInstance);
         return faceAPIInstance;
       })
-      .catch(err => console.log(err))
-    
+      .catch(err => console.log(err));
+
   } catch(error) {
     response.status(500).send('Sorry all berries');
   }
 }
 
 function dominantAttribute(data) {
-let emotionObj = data.faces[0].attributes.emotion
-let emotionArr = Object.entries(emotionObj);
-let final = emotionArr.reduce((previousValue, currentValue) => {
-  if (previousValue[1] > currentValue[1]) {
-    return previousValue;
-  } else {
-    return currentValue;
-  }
-})[0];
-return final;
-} 
+  let emotionObj = data.faces[0].attributes.emotion;
+  let emotionArr = Object.entries(emotionObj);
+  let final = emotionArr.reduce((previousValue, currentValue) => {
+    if (previousValue[1] > currentValue[1]) {
+      return previousValue;
+    } else {
+      return currentValue;
+    }
+  })[0];
+  return final;
+}
 
-function lotto()  {
+function lotto() {
   let arr = [];
   for (let i = 0; i < 6; i++) {
-    arr.push(Math.floor(Math.random() * (64 - 1)) + 1)
+    arr.push(Math.floor(Math.random() * (64 - 1)) + 1);
   }
   return arr.join(', ');
 }
@@ -93,7 +94,7 @@ function fortunePicker(dominant_attribute) {
   if (dominant_attribute === 'anger') {
     return 'This is an angry fortune';
   } else {
-  return 'This is not an angry fortune';
+    return 'This is not an angry fortune';
   }
 }
 
